eea76314079b8b2f157cd5ca158e5e478074512b
Systems Administrator <akyazze>
2024-11-22 08:37:45 +0300

    Complteted Drivers & Vehicles Modules

--- fleetApp/urls.py
+++ fleetApp/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
          # Main view
-    path('', views.main_view, name='main'),
+   # path('', views.main_view, name='main'),
     
          # Home urls
-    path('home/', views.home_view, name='home'),
+    path('', views.home_view, name='home'),
     
          ############################################################################# Vehicle urls ###########################
     path('vehicles/', views.vehicle_view, name='vehicle'),
